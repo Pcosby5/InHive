fcd54a95b6edfa9f676b67b3bdef5b49f5127437
Pcosby5 <pcosby50@gmail.com>
2024-03-06 06:33:08 +0100

    TaskSerializers

--- inHive/serializers.py
+++ inHive/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Hive
+from .models import Hive, Task
 
 class HiveSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hive
         fields = ['id', 'hiveTitle', 'hiveDescription', 'intendedUsers', 'hiveStartDate', 'hiveEndDate', 'status',]
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'taskTitle', 'taskDescription', 'taskStartDate', 'taskEndDate', 'assignedTo']
+
