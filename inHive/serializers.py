ff3500b02b5c34372a50b5861746418ab8b23ba7
Pcosby5 <pcosby50@gmail.com>
2024-03-06 06:37:59 +0100

    TaskSerializers

--- inHive/serializers.py
+++ inHive/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework import serializers
 from .models import Hive, Task
 
+# Create HiveSerializers to convert object into json
 class HiveSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hive
         fields = ['id', 'hiveTitle', 'hiveDescription', 'intendedUsers', 'hiveStartDate', 'hiveEndDate', 'status',]
 
+
+# Create TaskSerializers to convert object into json
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'taskTitle', 'taskDescription', 'taskStartDate', 'taskEndDate', 'assignedTo']
+
 
